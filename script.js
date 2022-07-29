1a62d7bd884f381a0f7f730f3e42f2055bc7756e
Praveen Kumar <81506220+p01maurya@users.noreply.github.com>
2022-07-29 21:15:17 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,100 @@
+function addNewWEField()
+{
+    // console.log("Hello");
+
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("weField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter Here");
+
+    //finding reference to know where to inset the above node
+    let weOb = document.getElementById("we");
+    let weAddButtonOb = document.getElementById("weAddButton");
+
+     weOb.insertBefore(newNode, weAddButtonOb);  //inserting newNode, before weAddButtonOb in weOb
+}
+
+
+function addNewAQField()
+{
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("eqField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter Here");
+
+    //finding reference to know where to inset the above node
+    let aqOb = document.getElementById("aq");
+    let aqAddButtonOb = document.getElementById("aqAddButton");
+
+     aqOb.insertBefore(newNode, aqAddButtonOb); 
+}
+
+
+
+//Generating Resume
+
+function generateResume(){
+    let nameField = document.getElementById("nameField").value;
+    let nameT1 = document.getElementById("nameT1");
+    nameT1.innerHTML = nameField;
+
+    document.getElementById('nameT2').innerHTML = nameField;
+
+    document.getElementById('contactT').innerHTML = document.getElementById("contactField").value;
+
+    // Important Links
+    document.getElementById('linkedT').innerHTML = document.getElementById("LinkedInField").value;
+    document.getElementById('gitT').innerHTML = document.getElementById("GitHubField").value;
+    document.getElementById('leetT').innerHTML = document.getElementById("cField").value;
+
+    //Objective
+    document.getElementById("objectiveT").innerHTML = document.getElementById("objectiveField").value;
+
+    //Work Experience
+    let wes = document.getElementsByClassName('weField')
+    let str=''
+
+    for(e of wes){
+        str = str + `<li> ${e.value} </li>`;
+    }
+
+    document.getElementById('weT').innerHTML = str;
+
+
+    //Academic Qualification
+    let aqs = document.getElementsByClassName('eqField')
+    let str1=''
+
+    for(e of aqs){
+        str1 += `<li> ${e.value} </li>`;
+    }
+
+    document.getElementById('aqT').innerHTML = str1;
+
+
+    //Code for setting image
+    let file = document.getElementById("imgField").files[0]
+
+    let reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    //Set image to template
+    reader.onloadend = function(){
+            document.getElementById('imgTemplate').src = reader.result;
+    };
+
+
+    document.getElementById('resume-form').style.display='none';
+    document.getElementById('resume-template').style.display='block';
+}
+
+
+//Print Resume
+function printResume(){
+    window.print();
+}
